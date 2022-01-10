73cc4a260938668428ae14f188430963da948b9d
이예슬 <20152917@sungshin.ac.kr>
2022-01-10 23:31:10 +0900

    update

--- src/main/java/com/example/effectivejava/EffectiveJavaApplication.java
+++ src/main/java/com/example/effectivejava/EffectiveJavaApplication.java
@@ -1,13 +1,17 @@
 package com.example.effectivejava;
 
+import com.example.effectivejava.ch03.Singleton;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EffectiveJavaApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException {
         SpringApplication.run(EffectiveJavaApplication.class, args);
+
+        Singleton singleton = Singleton.INSTANCE;
+
     }
 
 }
